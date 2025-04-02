d7ad923d8d5cfa2b59ae2fb9450a04dc94a8f737
Devocolypse <72180176+Devocolypse@users.noreply.github.com>
2025-04-02 09:15:44 -0700

    feat: Generate arbitrary # of squares

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-const squaresContainer = document.querySelector('.squaresContainer');
-
 // obj: create a function that displays a number of squares
 function displaySquares(squares) {
-  console.log(squares);
+  const squaresContainer = document.querySelector('.squaresContainer');
   squaresContainer.appendChild(squares);
 }
 
-// obj: create a function that creates one square
-function createSquares() {
-  const square = document.createElement('div');
-  square.classList.toggle('square');
+// obj: create a function that creates number of squares < 100
+function createSquares(input = 16) {
+  if (input < 1 || input > 100) {
+    return alert('Number of squares must be between (inclusively) 1 and 100.');
+  }
 
-  displaySquares(square);
+  // temporary container for generated squares
+  const container = document.createDocumentFragment();
+
+  for (let i = 0; i < input; i++) {
+    const square = document.createElement('div');
+    square.classList.toggle('square');
+
+    container.appendChild(square);
+  }
+
+  displaySquares(container);
 }
 
 createSquares();
