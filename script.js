0b37cb5b166c5b0dc063eb51a21080e0c7537154
Devocolypse <72180176+Devocolypse@users.noreply.github.com>
2025-04-03 10:07:29 -0700

    docs: Clarify src code instructions

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-// obj: create a function that displays a number of squares
 function displaySquares(squares) {
   const squaresContainer = document.querySelector('.squaresContainer');
   squaresContainer.replaceChildren(squares);
 }
 
-// obj: create a function that creates number of squares < 100
 function createSquares(input = 16) {
-  // temporary container for generated squares
   const container = document.createDocumentFragment();
 
-  // loop until input * input number of squares are generated
+  // create enough squares for an input*input grid
   for (let i = 1; i <= input * input; i++) {
     console.log(i, input);
     const square = document.createElement('div');
@@ -22,7 +19,7 @@ function createSquares(input = 16) {
 
     container.appendChild(square);
 
-    // break into new row every nth square, n = input
+    // break into new row every nth square in the row; n = input
     if (i % input === 0) {
       const breaker = document.createElement('div');
       breaker.classList.toggle('break');
@@ -46,6 +43,7 @@ function validateGridCreation(input) {
   createSquares(input);
 }
 
+// grid creation prompt
 const newGridBtn = document.querySelector('.newGridBtn');
 newGridBtn.addEventListener('click', () => {
   const answer = prompt('How many squares per side do you want?', 16);
